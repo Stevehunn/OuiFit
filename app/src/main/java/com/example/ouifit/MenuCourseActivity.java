5d7848552dba474eccc8caf1095547ae88c703a4
stevehunn <nicolas.bitaillou@etu.univ-tours.fr>
2021-03-12 09:32:48 +0100

    Ajout de la classe MenuCourseActivity et du layout menu_course.xml
    Début de l'initialisation de MenuCourseActivity
    
    Presque fini layout menu_course.xml

---
+++ app/src/main/java/com/example/ouifit/MenuCourseActivity.java
@@ -0,0 +1,12 @@
+package com.example.ouifit;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MenuCourseActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_course);
+    }
+}
