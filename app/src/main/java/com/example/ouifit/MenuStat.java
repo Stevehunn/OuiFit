bc412388bc23889108ac142684340292c2cd9e6b
stevehunn <nicolas.bitaillou@etu.univ-tours.fr>
2021-03-15 15:39:25 +0100

    MenuStat lier à son layout.

---
+++ app/src/main/java/com/example/ouifit/MenuStat.java
@@ -0,0 +1,13 @@
+package com.example.ouifit;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MenuStat extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_stat);
+    }
+}
