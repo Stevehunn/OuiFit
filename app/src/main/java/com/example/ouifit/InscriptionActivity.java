adfab5e2c65b19bb03676a827debcca61df1eae5
Jonas <jonas.trochet37@gmail.com>
2021-03-16 08:57:29 +0100

    Ajout inscription et classe correspondante

---
+++ app/src/main/java/com/example/ouifit/InscriptionActivity.java
@@ -0,0 +1,22 @@
+package com.example.ouifit;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class InscriptionActivity extends AppCompatActivity {
+
+    private EditText identifiant;
+    private EditText email;
+    private EditText mdp1;
+    private EditText mdp2;
+    private Button bouton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inscription);
+    }
+}
