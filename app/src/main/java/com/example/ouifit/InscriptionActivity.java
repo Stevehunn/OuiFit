fcb17b3d24474818ad1d00dc21ddcdfad7249487
Jonas <jonas.trochet37@gmail.com>
2021-03-16 09:36:28 +0100

    commencer inscription

--- app/src/main/java/com/example/ouifit/InscriptionActivity.java
+++ app/src/main/java/com/example/ouifit/InscriptionActivity.java
@@ -1,6 +1,7 @@
 package com.example.ouifit;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -18,5 +19,31 @@ public class InscriptionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.inscription);
-    }sjgdvcsgqsbsbs
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        if(view.getId() == R.id.boutonID)
+        {
+            String identifiantText = identifiant.getText().toString();
+            String emailText = email.getText().toString();
+            String MDP1Text = mdp1.getText().toString();
+            String MDP2Text = mdp2.getText().toString();
+            boolean concordance = MDP1Text.equals(MDP2Text);
+            if (concordance)
+            {
+                try {
+                    User user = new User(identifiantText,emailText,MDP1Text);// on doit créer un objet User permettant de sauvegarder les données d'un utilisateur
+                    //On doit ajouter les utilisateurs à la BDD
+
+                    //remise à zéro des champs du formaulaire
+                    identifiant.setText(null);
+                    email.setText(null);
+                    mdp1.setText(null);
+                    mdp2.setText(null);
+                }
+            }
+        }
+    }
 }
