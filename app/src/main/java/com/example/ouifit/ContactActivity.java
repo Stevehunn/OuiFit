1f9fde53716ca26a78b856eb6289bb777d7b336c
stevehunn <33178230+stevehunn@users.noreply.github.com>
2021-03-21 10:30:14 +0100

    Ajout de la page ContactActivity

---
+++ app/src/main/java/com/example/ouifit/ContactActivity.java
@@ -0,0 +1,28 @@
+package com.example.ouifit;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class ContactActivity extends Activity {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.contact);
+
+        //Bouton de retour pour aller vers le menu Exercice
+        Button buttonRetour = (Button) findViewById(R.id.btRetourEx1);
+        buttonRetour.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.i("Bouton exo 1", "execute");
+                Intent i = new Intent(ContactActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+    }
+}
