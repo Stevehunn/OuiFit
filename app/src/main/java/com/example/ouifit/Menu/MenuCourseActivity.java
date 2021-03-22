2fa455d606ae73b64d8de68cf5e36a3be33efb31
stevehunn <33178230+stevehunn@users.noreply.github.com>
2021-03-22 18:15:22 +0100

    Ajout Permission

--- app/src/main/java/com/example/ouifit/Menu/MenuCourseActivity.java
+++ app/src/main/java/com/example/ouifit/Menu/MenuCourseActivity.java
@@ -1,15 +1,28 @@
 package com.example.ouifit.Menu;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 
 import com.example.ouifit.MainActivity;
 import com.example.ouifit.R;
 
-public class MenuCourseActivity extends Activity {
+public class MenuCourseActivity extends Activity implements LocationListener {
+
+    private static final int PERMS_CALL_ID = 1234;
+    private LocationManager lm;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,7 +41,7 @@ public class MenuCourseActivity extends Activity {
         });
 
         //Bouton pour aller vers le menu stat
-        Button buttonStat = (Button)findViewById(R.id.btversStat2);
+        Button buttonStat = (Button) findViewById(R.id.btversStat2);
         buttonStat.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent i = new Intent(MenuCourseActivity.this, MenuStatActivity.class);
@@ -38,7 +51,7 @@ public class MenuCourseActivity extends Activity {
         });
 
         //Bouton pour aller vers le menu exercice
-        Button buttonExercice = (Button)findViewById(R.id.btversExercice2);
+        Button buttonExercice = (Button) findViewById(R.id.btversExercice2);
         buttonExercice.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent i = new Intent(MenuCourseActivity.this, MenuExercicesActivity.class);
@@ -51,9 +64,70 @@ public class MenuCourseActivity extends Activity {
     }
 
     /*------------------------Cycle de vie de l'activité-----------------------*/
-    protected void onPause() {
-        super.onPause();
-        finish();
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        checkPermissions();
     }
 
+    private void checkPermissions() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+            }, PERMS_CALL_ID);
+            return;
+        }
+
+        lm = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (lm.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, this);
+        }
+        if (lm.isProviderEnabled(LocationManager.PASSIVE_PROVIDER)) {
+            lm.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER, 1000, 0, this);
+        }
+        if (lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+            lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 0, this);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMS_CALL_ID) {
+            checkPermissions();
+        }
+    }
+
+    protected void onPause() {
+        super.onPause();
+        if (lm != null) {
+            lm.removeUpdates(this);
+        }
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        double latitude = location.getLatitude();
+        double longitude = location.getLongitude();
+
+        Toast.makeText(this, "Location: " + latitude + "/" + longitude, Toast.LENGTH_LONG).show();
+    }
 }
