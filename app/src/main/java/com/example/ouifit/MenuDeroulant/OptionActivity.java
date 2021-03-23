3e0ee98332e4c9fcbd855f9b2bf09fdf58bd998b
stevehunn <33178230+stevehunn@users.noreply.github.com>
2021-03-23 14:17:15 +0100

    Ajout Thème Ajout Bouton retour dans option.xml

--- app/src/main/java/com/example/ouifit/MenuDeroulant/OptionActivity.java
+++ app/src/main/java/com/example/ouifit/MenuDeroulant/OptionActivity.java
@@ -1,14 +1,32 @@
 package com.example.ouifit.MenuDeroulant;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.ouifit.MainActivity;
 import com.example.ouifit.R;
 
 public class OptionActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.option);
+
+
+        /*------------------------BOUTON-----------------------*/
+
+        //Bouton de retour pour aller vers le menu Exercice
+        Button buttonRetour = (Button) findViewById(R.id.btRetourOption);
+        buttonRetour.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.i("Bouton exo 1", "execute");
+                Intent i = new Intent(OptionActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
 
     }
 
