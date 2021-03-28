2ba1bd8900f8373cdea7422a29539c54879d04d2
stevehunn <33178230+stevehunn@users.noreply.github.com>
2021-03-29 01:43:24 +0200

    Ajout page profil.xml + ProfilActivity.java Modification style et quelques changements estétiques

---
+++ app/src/main/java/com/example/ouifit/MenuDeroulant/ProfilActivity.java
@@ -0,0 +1,51 @@
+package com.example.ouifit.MenuDeroulant;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.ouifit.MainActivity;
+import com.example.ouifit.R;
+
+public class ProfilActivity extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profil);
+
+        /*------------------------BOUTON-----------------------*/
+
+        //Bouton de retour pour aller vers le menu Exercice
+        Button buttonRetour = (Button) findViewById(R.id.btRetourProfil);
+        buttonRetour.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.i("Bouton retour profil", "execute");
+                Intent i = new Intent(ProfilActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+        //Bouton pour sauvegarder les données rentrée
+        Button buttonSave = (Button) findViewById(R.id.btSauvegarder);
+        buttonSave.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.i("Sauvegarde", "execute");
+                Intent i = new Intent(ProfilActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+    }
+
+    /*------------------------Cycle de vie de l'activité-----------------------*/
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+
+}
+
+
